4fea7c015ad2a24e1b69e74dccfed2b5b47bdd82
Christophe Hamerling <chamerling@linagora.com>
2017-03-23 08:08:30 +0100

    SOC-31 Use INBOX_GITHUB_TYPE constant

--- frontend/app/components/events/events.controller.js
+++ frontend/app/components/events/events.controller.js
@@ -4,10 +4,10 @@
   angular.module('linagora.esn.unifiedinbox.github')
     .controller('inboxGithubEventsController', inboxGithubEventsController);
 
-  function inboxGithubEventsController($stateParams, _, session, infiniteScrollHelper, inboxProviders, PageAggregatorService, sortByDateInDescendingOrder, ELEMENTS_PER_PAGE, INBOX_GITHUB_SUPPORTED_EVENTS) {
+  function inboxGithubEventsController($stateParams, _, session, infiniteScrollHelper, inboxProviders, PageAggregatorService, sortByDateInDescendingOrder, ELEMENTS_PER_PAGE, INBOX_GITHUB_SUPPORTED_EVENTS, INBOX_GITHUB_TYPE) {
     var self = this;
 
-    self.account = _.find(session.getProviderAccounts('github'), { username: $stateParams.username });
+    self.account = _.find(session.getProviderAccounts(INBOX_GITHUB_TYPE), { username: $stateParams.username });
     self.isSupported = isSupported;
 
     self.loadMoreElements = infiniteScrollHelper(self, function() {
@@ -16,7 +16,7 @@
       }
 
       return inboxProviders.getAll({
-        acceptedIds: ['github-' + self.account.id]
+        acceptedIds: [INBOX_GITHUB_TYPE + '-' + self.account.id]
       }).then(function(providers) {
         self.aggregator = new PageAggregatorService('unifiedInboxGithubControllerAggregator', providers, {
           compare: sortByDateInDescendingOrder,
